f718324bf86e6dca4a4e0324b6be327f0f9ac9c3
aleksvvv <adm-shop-001@yandex.ru>
2023-04-12 11:38:54 +0300

    withContext and suspendCoroutine

--- app/src/main/java/com/bignerdranch/android/myfactorial/MainViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/myfactorial/MainViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
     private val _state = MutableLiveData<State>()
@@ -21,10 +26,32 @@ class MainViewModel : ViewModel() {
         }
         viewModelScope.launch {
             val calLong = number.toLong()
-//            calculate
-            delay(5000)
-            _state.value = Resulting(number.toString())
+            val result = factorial(calLong)
+//            delay(1000)
+            _state.value = Resulting(result)
         }
     }
 
+    private suspend fun factorial(number: Long): String {
+
+      return withContext(Dispatchers.Default){
+           var result = BigInteger.ONE
+           for (i in 1..number) {
+               result = result.multiply(BigInteger.valueOf(i))
+           }
+          result.toString()
+       }
+    }
+//    private suspend fun factorial(number: Long): BigInteger {
+//
+//        return suspendCoroutine {
+//            thread {
+//                var result = BigInteger.ONE
+//                for (i in 1..number) {
+//                    result = result.multiply(BigInteger.valueOf(i))
+//                }
+//                it.resumeWith(Result.success(result))
+//            }
+//        }
+//    }
 }
